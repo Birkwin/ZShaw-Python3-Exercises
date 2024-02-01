6f241a284bd649872dee56440c97f2fc641241fa
Birkwin <140597289+Birkwin@users.noreply.github.com>
2024-02-01 15:17:25 +1030

    Exercise 12 done

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# Do the same as before, but ask the question and print the string all at once
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+# Format a string that includes the users answers and print it
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
